90d383ff9af45f15111258373d8ca5c8c2648860
Shwetha K M <shmurali98@gmail.com>
2019-04-25 17:39:22 +0530

    final driver

--- driver.py
+++ driver.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import shutil
 import pickle
-import glob
 from temp_detect import TempDetect
 from send_sms import SendSMS
 from send_mail import SendEmail
@@ -16,14 +14,13 @@ class Driver():
 		self.mailobj = SendEmail()
 
 	def get_image(self):
-		list_of_files = glob.glob('/home/pi/Pictures/server/dark/*')
-		latest_file = max(list_of_files, key=os.path.getctime)
+		latest_file = '/home/pi/Pictures/server/image.jpg'
 		return latest_file
 
 	def detect_digit(self):
 		subject = 'Temperature Alert'
 		negmsg = 'Unable to detect successfully. Detects: '
-		alertmsg = 'Temperature beyond acceptable range. The display reads: '
+		alertmsg = 'Temperature beyond acceptable range. The display reads : '
 		img_path = self.get_image()
 		tobj = TempDetect(img_path=img_path)
 		digits = []
@@ -31,47 +28,41 @@ class Driver():
 		try:
 			# First attempt to detect. cv2.dilate() value = 1
 			digits = tobj.final_call(iterval=1)
-			digits_str = u'{}{}'.format(*digits)
-
-			# Unable to map to a digit (returns -1)
-			if -1 in digits:
+			#digits2 = digits[:2]
+			digits_str = ''.join(str(x) for x in digits)
+			# Unable to map to a digit (returns -1) or detects no digit
+			if -1 in digits or len(digits)<1:
 				# Second attempt to detect. cv2.dilate() value = 2
 				digits = tobj.final_call(iterval=2)
-				digits_str = u'{}{}'.format(*digits)
-
+				digits_str = ''.join(str(x) for x in digits)
 				# Unable to detect second time
-				if -1 in digits:
+				if -1 in digits or len(digits)<1:
+					message = negmsg+(digits_str[:3])
 					for key in self.contacts_dict:
-						self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, negmsg, img_path)
-						self.smsobj.sendSMS(negmsg, self.contacts_dict[key]['mnum'])
+						if self.contacts_dict[key]['status'] != 'Inactive':
+							self.mailobj.sendMail(self.contacts_dict[key]['mailid'], subject, message, img_path)
+							if self.contacts_dict[key]['mnum'] != '':
+								self.smsobj.sendSMS(message, self.contacts_dict[key]['mnum'])
 					sys.exit()
 
 				#Else, successfully detected (2nd attempt)
 			#Else, successfully detected (1st attempt)
+			message = alertmsg+(digits_str[:3])
+			for key in self.contacts_dict:
+				if self.contacts_dict[key]['status'] != 'Inactive':
+					if not self.contacts_dict[key]['mint'] < digits_str < self.contacts_dict[key]['maxt']:
+						self.mailobj.sendMail(self.contacts_dict[key]['mailid'], subject, message, img_path)
+						if self.contacts_dict[key]['mnum'] != '':
+							self.smsobj.sendSMS(message, self.contacts_dict[key]['mnum'])
 
-		# Unable to detect both the digits correctly
-		except IndexError:
-			digits = tobj.final_call(iterval=2)
-			# Still unable to detect 2nd time (either -1 or misses one digit)
-			if -1 in digits or len(digits)<2:
-				for key in self.contacts_dict:
-					self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, negmsg, img_path)
-					if self.contacts_dict[key]['mnum'] != '':
-						self.smsobj.sendSMS(negmsg, self.contacts_dict[key]['mnum'])
+					else:
+						# Within the given range. Do nothing.
+						pass
 
-
-		digits2 = digits[:2]
-		digits_str = ''.join(str(x) for x in digits2)
-
-		for key in self.contacts_dict:
-        	if not self.contacts_dict[key]['mint'] < digits_str < self.contacts_dict[key]['maxt']:
-	            self.mailobj.sendEmail(self.contacts_dict[key]['mailid'], subject, alertmsg, moved_to)
-	            if self.contacts_dict[key]['mnum'] != '':
-					self.smsobj.sendSMS(alertmsg, self.contacts_dict[key]['mnum'])
-
-			else:
-				# Within the given range. Do nothing.
-				pass
+		except Exception as exception:
+			msg = 'ERROR: ' + type(exception).__name__
+			self.mailobj.sendMail(self.contacts_dict['Shwetha']['mailid'], subject, msg, img_path)
+			sys.exit()
 
 
 
