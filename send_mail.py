071c96a81f1c4a7d7d465596c754933d286c0434
geethika234 <31622964+geethika234@users.noreply.github.com>
2019-07-22 11:24:08 +0530

    Delete send_mail.py

--- send_mail.py
+++
@@ -1,9 +0,0 @@
-from email_handler import ClassEmail
-
-class SendEmail:
-	def __init__(self):
-		self.email = ClassEmail()
-
-	def sendMail(self, to_add, subject, message, imgfile):
-		self.email.send_Image_Mail(to_add, subject, message, imgfile)
-		del self.email
